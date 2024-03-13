adaab42814fef341127bb02ed240c5819d97fbc9
JAYAKRISHNA YELLAKARA <yjayakrish@gmail.com>
2024-03-13 23:50:17 +0530

    removed some again

--- src/CodeSection.jsx
+++ src/CodeSection.jsx
@@ -1,34 +1,10 @@
 import "./CodeSection.css";
-// import {useRef, useEffect} from 'react';
 function CodeSection(props){
-  // const codeRef=useRef(null);
-  // let Content;
-  // useEffect(()=>{
-  //   if(codeRef.current){
-  //      Content=codeRef.current.innerText;
-  //   }
-
-  // },[]);
-  // function copyToClipboard() {
-  //   var tempInput = document.createElement("input");
-  //   tempInput.value = Content;
-  //   document.body.appendChild(tempInput);
-  //   tempInput.select();
-  //   document.execCommand("copy");
-  //   document.body.removeChild(tempInput);
-  
-  //   alert("Content copied to clipboard!");
-  // }
-// section div onClick={copyToClipboard}
-// code div ref={codeRef}
   return(
     <div className="Section" >
 <div className="Title">
 {` ${props.name}`}
 </div>
-{/* <div className="Code" ref={codeRef}>
-{props.code}
-</div> */}
 <pre className="Code" >
   {props.code}
 </pre>
@@ -36,5 +12,3 @@ function CodeSection(props){
   );
 }
 export default CodeSection;
-
-////
--- src/CreateSection.jsx
+++ src/CreateSection.jsx
@@ -1,10 +1,7 @@
 import CodeSection from "./CodeSection.jsx";
-import {CopyToClipboard} from 'react-copy-to-clipboard';
 function CreateSection(props){
 return(
-    <CopyToClipboard key={props.id} text={props.code}>
     <CodeSection key={props.id} name={props.name} code={props.code}/>
-    </CopyToClipboard>
 );
 }
 export default CreateSection;
