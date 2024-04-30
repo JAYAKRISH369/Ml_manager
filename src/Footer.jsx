7adf9a13dcced12bdde9a8672935c94ea656bd88
JAYAKRISHNA YELLAKARA <yjayakrish@gmail.com>
2024-05-01 00:08:45 +0530

    again changed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,9 @@
 import "./CodeSection.css";
 function Footer(){
     return(
-        <>
-        <div className="Footer_div">
-            Jk presents..
-        </div>
         <div className="Footer_div">
         It's your Healper... go_to_Hell
         </div>
-        </>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import ans from "./answers.jsx";
 import Footer from "./Footer.jsx"
 ReactDOM.createRoot(document.getElementById('root')).render(
     <div>
-    <Footer />
+    <button><a href="./finddataset.csv" download>find-s data set</a></button>
+    <button><a href="./finddataset.csv" download>Candidate Elimination data set</a></button>
+    <button><a href="./tennis.csv" download>ID 3 data set</a></button>
  {ans.map(CreateSection)}
-
+ 
  <Footer />
  </div>
 );
